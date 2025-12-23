8e8eb771518a2c1c9b95cd30256cc1e5aeaaa576
gabus2194 <dg22023@ues.edu.sv>
2025-12-23 15:50:14 -0600

    Primera prueba para produccion

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    apiUrl: 'PRODUCTION_API_URL', // Reemplaza con la URL de tu backend en producción
+    //auth: {
+      //domain: 'YOUR_AUTH0_DOMAIN', // Reemplaza con tu Dominio de Auth0
+      //clientId: 'YOUR_AUTH0_CLIENT_ID', // Reemplaza con tu Client ID de Auth0
+     // authorizationParams: {
+       // audience: 'YOUR_API_IDENTIFIER', // Reemplaza con el Identificador (Audience) de tu API
+        //redirect_uri: window.location.origin,
+      //},
+      //namespace: 'https://minibooking.com/roles', // Reemplaza con tu Namespace de Auth0 para roles
+    //}
+};
