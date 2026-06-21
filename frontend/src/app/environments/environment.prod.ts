f5b61985866c62beda3e66e205514f947874c095
Gabusnow21 <dgabriel.123.gd@gmail.com>
2026-06-21 16:58:25 -0600

    Configuracion para despliegue usando traefik dockerizado

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,13 +1,11 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://back-administrador-de-notas-matematicas.onrender.com/api', // Reemplaza con la URL de tu backend en producción
+    apiUrl: 'https://api.gestor.edumathsv.work', // Reemplaza con la URL de tu backend en producción
     //auth: {
       //domain: 'YOUR_AUTH0_DOMAIN', // Reemplaza con tu Dominio de Auth0
       //clientId: 'YOUR_AUTH0_CLIENT_ID', // Reemplaza con tu Client ID de Auth0
      // authorizationParams: {
        // audience: 'YOUR_API_IDENTIFIER', // Reemplaza con el Identificador (Audience) de tu API
         //redirect_uri: window.location.origin,
-      //},
-      //namespace: 'https://minibooking.com/roles', // Reemplaza con tu Namespace de Auth0 para roles
-    //}
+      //}
 };
